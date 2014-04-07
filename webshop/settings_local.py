efa4ea354adaaa42480330a48de98599b4202511
Aleksandr <greenteamer@bk.ru>
2014-04-07 17:00:15 +0700

    добавил settings_local в ignore

--- webshop/settings_local.py
+++ webshop/settings_local.py
@@ -4,7 +4,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'clothes',                      # Or path to database file if using sqlite3.
         'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': 'qaw',                  # Not used with sqlite3.
+        'PASSWORD': 'balabas',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
