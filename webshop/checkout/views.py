2c7476cc4c2a44912c9d0d9a861e5d02bee66a2d
greenteamer <greenteamer@bk.ru>
2014-11-02 01:20:23 +0300

    robokassa ok

--- webshop/checkout/views.py
+++ webshop/checkout/views.py
@@ -142,31 +142,31 @@ def payment_received(sender, **kwargs):
     order.save()
 
     """отправка писем"""
-    # order_items = OrderItem.objects.filter(order=order)
-    # items = ''
-    # for item in order_items:
-    #     items = items + '%s \n' % item.name
-    # if order.payment_method == 1:
-    #     payment_method = u'Оплата курьером'
-    # else:
-    #     payment_method = u'Оплата онлайн'
-    # subject = u'podarkoff-moscow.ru заявка от %s' % order.shipping_name
-    # message = u'Заказ №: %s \n Имя: %s \n телефон: %s \n почта: %s \n id заказа: %s \n Товары: %s \n Стоимость: %s \n Способ оплаты: %s' % (order.transaction_id, order.shipping_name, order.phone, order.email, order.id, items, order.total, payment_method)
-    # send_mail(subject, message, 'teamer777@gmail.com', [ADMIN_EMAIL], fail_silently=False)
-    #
-    # context_dict = {
-    #         'transaction': order.transaction_id,
-    #         'id': order.id,
-    #         'items': items,
-    #         'total': order.total,
-    #         'payment_method': payment_method,
-    #     }
-    #
-    # message = render_to_string('checkout/email.html', context_dict)
-    # from_email = 'teamer777@gmail.com'
-    # to = '%s' % order.email
-    # msg = EmailMultiAlternatives(subject, message, from_email, [to])
-    # msg.content_subtype = "html"
-    # msg.send()
+    order_items = OrderItem.objects.filter(order=order)
+    items = ''
+    for item in order_items:
+        items = items + '%s \n' % item.name
+    if order.payment_method == 1:
+        payment_method = u'Оплата курьером'
+    else:
+        payment_method = u'Оплата онлайн'
+    subject = u'podarkoff-moscow.ru заявка от %s' % order.shipping_name
+    message = u'Заказ №: %s \n Имя: %s \n телефон: %s \n почта: %s \n id заказа: %s \n Товары: %s \n Стоимость: %s \n Способ оплаты: %s' % (order.transaction_id, order.shipping_name, order.phone, order.email, order.id, items, order.total, payment_method)
+    send_mail(subject, message, 'teamer777@gmail.com', [ADMIN_EMAIL], fail_silently=False)
+
+    context_dict = {
+            'transaction': order.transaction_id,
+            'id': order.id,
+            'items': items,
+            'total': order.total,
+            'payment_method': payment_method,
+        }
+
+    message = render_to_string('checkout/email.html', context_dict)
+    from_email = 'teamer777@gmail.com'
+    to = '%s' % order.email
+    msg = EmailMultiAlternatives(subject, message, from_email, [to])
+    msg.content_subtype = "html"
+    msg.send()
 
 result_received.connect(payment_received)
